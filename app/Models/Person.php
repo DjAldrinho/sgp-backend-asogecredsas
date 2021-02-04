4a48d338c57f62d4c1cf231e1a0e2078fa4f785e
Dj Aldrinho <anarvaez@serempre.com>
2021-02-03 23:15:30 -0500

    Migaciones

---
+++ app/Models/Person.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'phone',
+        'user_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+
+}
