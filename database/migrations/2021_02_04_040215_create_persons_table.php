7533509486ea4b51216adff4d5a9a64d72c1d3e8
Dj Aldrinho <anarvaez@serempre.com>
2021-02-03 23:37:54 -0500

    Instalacion de passport

--- database/migrations/2021_02_04_040215_create_persons_table.php
+++ database/migrations/2021_02_04_040215_create_persons_table.php
@@ -15,9 +15,9 @@ class CreatePersonsTable extends Migration
     {
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('phone')->nullable();
+            $table->string('name')->comment('Nombre');
+            $table->string('surname')->comment('Apellido');
+            $table->string('phone')->nullable()->comment('Telefono');
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->softDeletes();
