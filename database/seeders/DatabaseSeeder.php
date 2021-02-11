839293860c95384296609b4617217ce76630e700
drzioner <drzioner@gmail.com>
2021-02-11 00:38:07 -0500

    UPDATE: se actualizaron los factories y seeders para incluir la data fake que faltaba par aun funcionamiento adecuado.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,7 +32,7 @@ class DatabaseSeeder extends Seeder
             CreditTypesSeeder::class
         ]);
 
-        if (env('APP_ENV') == 'local') {
+        if (config('app.env') == 'local') {
             User::factory(5)->create();
             Adviser::factory(10)->create();
             Client::factory(20)->create();
